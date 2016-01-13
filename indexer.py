cf482a6b0be309ecfa446a2568133de19c8ea6a6
Andres Vargas <andres.vargas@interalia.net>
2016-01-12 18:38:08 -0600

    indexer

--- indexer.py
+++ indexer.py
@@ -1,36 +1,29 @@
-import requests
-from webscraping import xpath 
+from nyaa import nyaa
+import guessit
+import sys
 
-FANSUBS=(
-  "158741",      
-)
+NYAA_USERS = {
+'hoshisora.moe':[158741,],
+'puya.se': [239789,],
+}
 
+def main():
 
-def scrape_nyaa_id(nyaa_id):
-    url_base = "http://www.nyaa.se/?user=%s&page=separate" % nyaa_id
-    res = requests.get(url_base).text
-    titles = xpath.search(res, "//td[@class='name']/span/text()")
-    tbody_complete = xpath.search(res, "//tbody[@class='hide']")
-    list_dict = []
-    for index,title in enumerate(titles):
-        tbody = tbody_complete[index]
-        rows= xpath.search(tbody,"//tr")
-        rows = rows[1:]
-        d = {'title': title, 'data':[]}
-        for row in rows:
-            part = xpath.get(row, "//td[1]/a/text()")
-            link = xpath.get(row, "//td[1]/a/@href")
-            type_ = xpath.get(row, "//td[2]/text()")
-            size = xpath.get(row, "//td[5]/text()")
-            crc = xpath.get(row, "//td[6]/text()")
-            row_dict = dict(part=part,link= link, type=type_, size=size, crc=crc)
-            d["data"].append(row_dict)
-        list_dict.append(d)
-    return list_dict
+    for fansub, users in NYAA_USERS.items():
+        for user in users:
+            offset = 1
+            while True:
+                results = nyaa.search(user=user,offset=offset)
+                if not results:
+                    break
+                offset +=1
+                for res in results:
+                    data = guessit.guessit(res.title)
+                    try:
+                        fansub,data["release_group"], data["title"]
+                    except KeyError:
+                        print data
 
 
-if __name__ == "__main__":
-    import json
-    for i in FANSUBS:
-        l = scrape_nyaa_id(i)
-        print json.dumps(l)
+if __name__=="__main__":
+    main()
